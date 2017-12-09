59d6c6c82edb3abbfa945597dbb12ade5c71535b
Remi Caumette <remicaumette@icloud.com>
2017-12-09 16:06:09 +0100

    Fix imports

--- app.js
+++ app.js
@@ -10,19 +10,19 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
 //Basics commands
-client.commands.set('help', require('./srcs/features/help.js'));
-client.commands.set('roll', require('./srcs/features/roll.js'));
-client.commands.set('rewards', require('./srcs/features/rewards.js'));
-client.commands.set('give', require('./srcs/features/give.js'));
-client.commands.set('remove', require('./srcs/features/remove.js'));
-client.commands.set('lang', require('./srcs/features/lang.js'));
+client.commands.set('help', require('./src/features/help.js'));
+client.commands.set('roll', require('./src/features/roll.js'));
+client.commands.set('rewards', require('./src/features/rewards.js'));
+client.commands.set('give', require('./src/features/give.js'));
+client.commands.set('remove', require('./src/features/remove.js'));
+client.commands.set('lang', require('./src/features/lang.js'));
 
 //All event when we use message event.
-client.on('guildMemberAdd', member => require('./srcs/events/guildMemberAdd.js')(client, member));
+client.on('guildMemberAdd', member => require('./src/events/guildMemberAdd.js')(client, member));
 //All event when the bot start
-client.on('ready', () => require('./srcs/events/ready.js')(client));
+client.on('ready', () => require('./src/events/ready.js')(client));
 //All event when we use message event.
-client.on('message', message => require('./srcs/events/message.js')(client, message));
+client.on('message', message => require('./src/events/message.js')(client, message));
 
 //Client token
-client.login(config.token);
+client.login(process.env.TOKEN || config.token);
